34022b54706a362c5ad7463a1fc892b70322e431
chenjr15 <chenjr15@live.cn>
2019-05-13 00:08:10 +0800

    添加输入法自动隐藏

--- app/src/main/java/dev/chenjr/tracer/LoginActivity.java
+++ app/src/main/java/dev/chenjr/tracer/LoginActivity.java
@@ -1,6 +1,7 @@
 package dev.chenjr.tracer;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
@@ -9,6 +10,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -49,13 +52,17 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.btn_login_login)
-    protected void onLoginClicked() {
+    protected void onLoginClicked(View view) {
         if (checkSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
             Log.i(TAG, "onLoginClicked: Granting Permission!");
             requestPermissions(new String[]{
                     Manifest.permission.INTERNET
             }, 1);
         }
+
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).
+                hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
         user = account.getText().toString();
         pass = password.getText().toString();
         if ("".equals(user)) {
