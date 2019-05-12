5b17b386df3570027085d9e0f61a9089d8758adc
chenjr15 <chenjr15@live.cn>
2019-05-12 16:00:41 +0800

    添加空的温度图Fragment

---
+++ app/src/main/java/dev/chenjr/tracer/fragment/TempGrapFragment.java
@@ -0,0 +1,28 @@
+package dev.chenjr.tracer.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import dev.chenjr.tracer.R;
+
+public class TempGrapFragment extends BaseFragment {
+    private Unbinder unbinder;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_temp_curve, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();//视图销毁时必须解绑
+    }
+}
